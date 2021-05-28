cc9d8297a93c82df9f1bc12d15f1ebaf4977bc0f
Pedro Prestes <pedrohenriqueprestesc@gmail.com>
2021-05-28 00:24:54 -0400

    alteracao no nome

--- EletricaBR/Properties/Settings.Designer.cs
+++ EletricaBR/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TCC.Properties {
+namespace EasyEletrica.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
